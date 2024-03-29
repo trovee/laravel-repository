b3019fee6f621e30f3d863c8d4b9604d852dbfb6
Utku Korkmaz <uutkukorkmaz@gmail.com>
2024-03-29 23:45:31 +0300

    Remove dummy test and add todo

--- tests/Unit/Managers/RegistryManagerTest.php
+++ tests/Unit/Managers/RegistryManagerTest.php
@@ -1,10 +1,3 @@
 <?php
 
-it('can find all repository candidates', function () {
-    $registry = app('repository.registry');
-
-    $invasion = invade($registry, fn() => $this->candidates, return: true);
-
-    expect($invasion)->toBeArray()
-        ->toHaveCountGreaterThan(0);
-});
+// todo: add tests
