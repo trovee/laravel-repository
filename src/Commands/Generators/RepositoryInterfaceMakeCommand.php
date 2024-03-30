f570cd97e0f102ac6520f636b37c897a75f2e552
Utku Korkmaz <uutkukorkmaz@gmail.com>
2024-03-30 14:32:55 +0300

    Add missing commands

---
+++ src/Commands/Generators/RepositoryInterfaceMakeCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Trovee\Repository\Commands\Generators;
+
+use Illuminate\Console\GeneratorCommand;
+
+class RepositoryInterfaceMakeCommand extends GeneratorCommand
+{
+    protected $name = 'make:repository-interface';
+
+    protected $description = 'Create a new repository interface';
+
+    protected $type = 'Repository';
+
+    protected function getStub()
+    {
+        return __DIR__.'/../../stubs/repository.interface.stub';
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Repositories';
+    }
+}
+{
+
+}
---
+++ src/Commands/InstallCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Trovee\Repository\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+
+    protected $signature = 'repository:install';
+
+    protected $description = 'Install Trovee Repository package';
+
+    public function handle()
+    {
+        $this->info('Installing Trovee Repository package...');
+
+        $this->info('Publishing configuration...');
+        $this->call('vendor:publish', [
+            '--provider' => "Trovee\Repository\RepositoryServiceProvider",
+            '--tag' => "config"
+        ]);
+
+        $this->info('Trovee Repository package installed successfully.');
+    }
+
+}
