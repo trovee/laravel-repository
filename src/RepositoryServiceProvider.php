4c7cec7643f479d6d0715d386a1ff508cf1018f1
Utku Korkmaz <uutkukorkmaz@gmail.com>
2024-03-30 14:30:43 +0300

    Add support for 'default' keyword in bindings

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace Trovee\Repository;
 
+use Closure;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Trovee\Repository\Commands\InstallCommand;
 use Trovee\Repository\Managers\RegistryManager;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -18,18 +21,34 @@ class RepositoryServiceProvider extends ServiceProvider
             __DIR__.'/../config/repository.php',
             'repository'
         );
+
+        $this->commands([
+            InstallCommand::class,
+            Commands\Generators\RepositoryInterfaceMakeCommand::class,
+        ]);
     }
 
     public function boot(): void
     {
         //        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->handleBindings();
+
+        Model::preventLazyLoading(
+            config('repository.should_be_strict', Model::preventsLazyLoading())
+        );
+        Model::preventAccessingMissingAttributes(
+            config('repository.should_be_strict', Model::preventsAccessingMissingAttributes())
+        );
+        Model::preventSilentlyDiscardingAttributes(
+            config('repository.should_be_strict', Model::preventsSilentlyDiscardingAttributes())
+        );
+
     }
 
-    protected function bootManager(): \Closure
+    protected function bootManager(): Closure
     {
         return function (Application $app) {
-            $manager = new RegistryManager($app);
+            $manager = $app->make(RegistryManager::class);
             $manager->boot();
 
             return $manager;
@@ -41,6 +60,9 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind('repository.registry', $this->bootManager());
 
         foreach (config('repository.bindings') as $abstract => $concrete) {
+            if ($concrete === 'default') {
+                continue;
+            }
             $this->app->bind($abstract, $concrete);
         }
 
