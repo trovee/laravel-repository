87018a4153c414386c8ee1fed81940312a7d18f0
Utku Korkmaz <uutkukorkmaz@gmail.com>
2024-03-30 14:34:13 +0300

    add test for dummy repository

--- tests/Feature/Facades/RepositoryTest.php
+++ tests/Feature/Facades/RepositoryTest.php
@@ -1,9 +1,18 @@
 <?php
 
+use Trovee\Repository\Contracts\RepositoryInterface;
 use Trovee\Repository\Facades\Repository;
 use Trovee\Repository\Managers\RepositoryManager;
+use Workbench\App\Models\User;
 
 it('can resolve the facade as RepositoryManager instance', function () {
     $repository = Repository::getFacadeRoot();
     expect($repository)->toBeInstanceOf(RepositoryManager::class);
+});
+
+it('can get UserRepository', function () {
+
+    $repository = Repository::get(User::class);
+
+    expect($repository)->not->toBeNull()->toBeInstanceOf(RepositoryInterface::class);
 });
