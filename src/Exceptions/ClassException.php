bea24a88c567b3c46397575fdb528a2b115dadca
Utku Korkmaz <uutkukorkmaz@gmail.com>
2024-03-30 14:29:17 +0300

    Remove unused static methods from ClassException

--- src/Exceptions/ClassException.php
+++ src/Exceptions/ClassException.php
@@ -2,34 +2,8 @@
 
 namespace Trovee\Repository\Exceptions;
 
-use Exception;
-use Illuminate\Database\Eloquent\Model;
-use Trovee\Repository\Contracts\RepositoryInterface;
+use RuntimeException;
 
-class ClassException extends Exception
+class ClassException extends RuntimeException
 {
-    public static function doesNotExists(string $fqcn, $previous = null)
-    {
-        return new static("Failed when trying to access $fqcn. Class does not exists.", 0, $previous);
-    }
-
-    public static function mustImplement(string $fqcn, string $interface, $previous = null)
-    {
-        return new static("Failed when trying to access $fqcn. Class must implement $interface.", 0, $previous);
-    }
-
-    public static function mustBeAnInstanceOf(string $fqcn, string $instance, $previous = null)
-    {
-        return new static("Failed when trying to access $fqcn. Class must be an instance of $instance.", 0, $previous);
-    }
-
-    public static function isNotModel(string $model)
-    {
-        return self::mustBeAnInstanceOf($model, Model::class);
-    }
-
-    public static function isNotRepository(string $repository)
-    {
-        return self::mustImplement($repository, RepositoryInterface::class);
-    }
 }
